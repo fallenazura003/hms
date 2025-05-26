8a193bd4611e8a35150a6944ff046ff67ec69816
fallenazura003 <fallenazura003@gmail.com>
2025-05-26 20:34:46 +0700

    Khởi tạo UI component cho header và sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '@mantine/core/styles.css'
+import {Button, createTheme, MantineProvider} from '@mantine/core';
+import AppRoutes from "./Routes/AppRoutes";
+const theme = createTheme({
+  focusRing:"never",
+  fontFamily:"Metal Mania, sans-serif",
+  headings:{fontFamily:"Metal Mania, sans-serif"},
+  colors: {
+    primary: ['#7AD1DD', '#5FCCDB', '#44CADC', '#2AC9DE', '#1AC2D9', '#11B7CD', '#09ADC3', '#0E99AC', '#128797', '#147885'],
+    neutral: ['#F0BBDD', '#ED9BCF', '#EC7CC3', '#ED5DB8', '#F13EAF', '#F71FA7', '#FF00A1', '#E00890', '#C50E82', '#AD1374'],
+  },
+  primaryColor:'primary',
+  primaryShade:4,
+  defaultGradient:{
+    from:"primary.4",
+    to:"primary.8",
+    deg:132
+  }
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-red-500">
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <MantineProvider theme={theme}>
+       <AppRoutes/>
+      </MantineProvider>
+
   );
 }
 
---
+++ src/Routes/AppRoutes.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function AppRoutes() {
+    return (
+        <div></div>
+    );
+}
+
+export default AppRoutes;
